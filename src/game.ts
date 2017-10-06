87cd06272cae0d26fce63992b1ec872c23fe269a
ualikhansars <valikhan754@gmail.com>
2017-10-06 06:01:59 -0700

    Fix mousemove action

--- src/game.ts
+++ src/game.ts
@@ -52,64 +52,30 @@ console.log('map', map);
 console.error('units', units);
 console.error('computersUnits', computersUnits);
 
-// canvas.addEventListener('click', (e:any) => {
-//   console.error('Click');
-//   let x = e.offsetX; // get X
-//   let y = e.offsetY; // get Y
-//   console.log('Position x', e.offsetX); // get X
-//   console.log('Position y', e.offsetY); // get Y
-//   onChooseUnit(units, x, y);
-//   console.log('currentlyChosenUnit', currentlyChosenUnit);
-// });
-
-// set onClickListener for right mouse event
-// canvas.addEventListener('contextmenu', (e:any) => {
-//   console.error('Right Mouse Click');
-//   e.preventDefault();
-//   let x = e.offsetX; // get X
-//   let y = e.offsetY; // get Y
-//   if(currentlyChosenUnit) {
-//     for(let computersUnit of computersUnits) {
-//       let bottomRightX = computersUnit.x + gridSize;
-//       let bottomRightY = computersUnit.y + gridSize;
-//       if(x >= computersUnit.x && x < bottomRightX && y >= computersUnit.y && y < bottomRightY) {
-//         console.log('computersUnit', computersUnit);
-//         currentlyChosenUnit.setUnitToPursue(computersUnit);
-//         let startNode = getNodeFromMap(currentlyChosenUnit.x, currentlyChosenUnit.y, map);
-//         let finishNode = getNodeFromMap(x, y, map);
-//         let path:any = aStar(map, startNode, finishNode);
-//         pursueUnit(currentlyChosenUnit, computersUnit, computersUnit.x, computersUnit.y, 0, path);
-//       } else {
-//         currentlyChosenUnit.setUnitToPursue(null);
-//         let startNode = getNodeFromMap(currentlyChosenUnit.x, currentlyChosenUnit.y, map);
-//         let finishNode = getNodeFromMap(x, y, map);
-//         let path:any = aStar(map, startNode, finishNode);
-//         console.error('startNode', startNode);
-//         console.error('finishNode', finishNode);
-//         assignUnitMoveToPosition(currentlyChosenUnit, finishNode.x, finishNode.y);
-//         updateUnit(currentlyChosenUnit,path, 0, finishNode.x, finishNode.y);
-//       }
-//     }
-//   }
-// });
-
-auxiliaryCanvas.onmousemove = (e:any) => {
+auxiliaryCanvas.addEventListener('mousemove', (e:any) => {
   let mouseX = e.offsetX; // get X
   let mouseY = e.offsetY; // get Y
   if(currentlyChosenUnit) {
+    let pointedUnit = null;
     for(let unit of computersUnits) {
-      let bottomRightX = unit.x + gridSize;
-      let bottomRightY = unit.y + gridSize;
-      if(mouseX >= unit.x && mouseX < bottomRightX && mouseY >= unit.y && mouseY < bottomRightY) {
-        auxiliaryCtx.fillStyle = '#000';
-        auxiliaryCtx.font = "15px Arial";
-        auxiliaryCtx.fillText("Attack", unit.x, unit.y + gridSize / 2);
-      } else {
-        auxiliaryCtx.clearRect(0, 0, WIDTH, HEIGHT);
+      let finishX = unit.x + gridSize;
+      let finishY = unit.y + gridSize;
+      if((mouseX >= unit.x && mouseX < finishX) && (mouseY >= unit.y && mouseY < finishY)) {
+        pointedUnit = unit;
       }
     }
+
+    if(pointedUnit) {
+      let finishX = pointedUnit.x + gridSize;
+      let finishY = pointedUnit.y + gridSize;
+      auxiliaryCtx.fillStyle = '#000';
+      auxiliaryCtx.font = "14px Arial";
+      auxiliaryCtx.fillText("Attack", pointedUnit.x, pointedUnit.y + gridSize / 2);
+    } else {
+       auxiliaryCtx.clearRect(0, 0, WIDTH, HEIGHT);
+    }
   }
-}
+});
 
 auxiliaryCanvas.addEventListener('click', (e:any) => {
   console.error('Click');
