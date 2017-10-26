ba07617fe3b5f164976012f51cf386174c48c21d
ualikhansars <valikhan754@gmail.com>
2017-10-26 03:36:32 -0700

    create calculateUnitsToBeware function

--- src/AI/analyzeModule/unitAnalyze.ts
+++ src/AI/analyzeModule/unitAnalyze.ts
@@ -1,5 +1,9 @@
-export const analyzeUnit = (unit:any):any => {
+import {
+  playersUnits
+} from '../../store/unitStore';
 
+export const analyzeUnit = (unit:any):any => {
+  console.log(calculateUnitsToBeware(unit));
 }
 
 /*
@@ -7,5 +11,17 @@ export const analyzeUnit = (unit:any):any => {
   and decide whether the unit require support or not
 */
 export const DoesUnitNeedSupport = (unit:any) => {
-  
+
+}
+
+export const calculateUnitsToBeware = (unit:any):number => {
+  let unitsToBeware:number = 0;
+  for(let enemy of playersUnits) {
+    for(let vulnerability of unit.vulnerableAgainst) {
+      if(vulnerability === enemy.name) {
+        unitsToBeware++;
+      }
+    }
+  }
+  return unitsToBeware;
 }
