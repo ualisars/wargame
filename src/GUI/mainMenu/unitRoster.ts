25d867b2dccd4a72a60c80b63dbbd2799538ebfe
ualikhansars <valikhan754@gmail.com>
2017-11-09 04:50:37 -0800

    display cost for roster

--- src/GUI/mainMenu/unitRoster.ts
+++ src/GUI/mainMenu/unitRoster.ts
@@ -24,24 +24,34 @@ const displayRosterTitle = () => {
   displayRosterUnits();
 }
 
-const displayRosterUnits = (i:number = 0, x:number=20, y:number=titleHeight+60) => {
+const displayRosterUnits = (i:number = 0, x:number=60, y:number=titleHeight+60) => {
   if(i >= roster.length) {
     return;
   }
-  const imgWidth:number = 90;
-  const imgHeight:number = 90;
+  const imgWidth:number = 80;
+  const imgHeight:number = 80;
   loadImage(roster[i].imgSrc, (err:any, img:any) => {
     mainMenuCtx.font = '14px serif';
     mainMenuCtx.fillStyle = '#000';
     mainMenuCtx.textAlign = 'left';
     mainMenuCtx.fillText(roster[i].name, x + 5, y);
     mainMenuCtx.drawImage(img, x, y + 5, imgWidth, imgHeight);
+    drawCoin(x + 15, y + imgHeight + 15, 5);
+    mainMenuCtx.fillStyle = '#000';
+    mainMenuCtx.fillText(roster[i].cost, x + 25 ,y + imgHeight + 20);
     x = (x + imgWidth + 15);
     if(i === 2 || i === 5 || i === 8 || i === 11) {
-      x = 20;
-      y += imgHeight + 25;
+      x = 60;
+      y += imgHeight + 45;
     }
     i++;
     displayRosterUnits(i, x, y);
   });
+}
+
+const drawCoin = (centerX:number, centerY:number, radius:number) => {
+  mainMenuCtx.beginPath();
+  mainMenuCtx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  mainMenuCtx.fillStyle = '#ffd700';
+  mainMenuCtx.fill();
 }
