a36604af1a2589dda96279d0231062c9d6281b90
ualikhansars <valikhan754@gmail.com>
2018-01-07 08:05:14 -0800

    create rearrangePlayerArmyPosition and rearrangeComputerArmyPosition in store/army

--- src/store/roster/army.ts
+++ src/store/roster/army.ts
@@ -60,10 +60,11 @@ export const removeUnitFromArmy = (unit:any) => {
     console.error('playerArmyPositionY', playerArmyPositionY);
     if(side === 'player') {
       playerArmy = deleteUnitFromArmy(unit, playerArmy);
+      rearrangePlayerArmyPosition();
     } else {
       computerArmy = deleteUnitFromArmy(unit, computerArmy);
+      rearrangeComputerArmyPosition();
     }
-    rearrangeArmyPosition();
     console.error('after deletion');
     console.error('playerArmyPositionX', playerArmyPositionX);
     console.error('playerArmyPositionY', playerArmyPositionY);
@@ -124,11 +125,69 @@ export const rearrangeArmyPosition = () => {
     }
     i++;
   }
-  if(side === 'player') {
+  if(side === 'player' && army.length !== 0) {
     playerArmyPositionX = army[army.length - 1].armyPosition.x;
     playerArmyPositionY = army[army.length - 1].armyPosition.y;
   } else {
     computerArmyPositionX = army[army.length - 1].armyPosition.x;
     computerArmyPositionY = army[army.length - 1].armyPosition.y;
+  }
+}
+
+export const rearrangePlayerArmyPosition = () => {
+  let marginWidth = armyImgWidth + 10;
+  let marginHeight = armyImgHeight + 15;
+  let defaultWidth = unitRosterWidth + 20;
+  let x:number= unitRosterWidth + 20;
+  let y:number= titleHeight + 60;
+  let i = 1;
+  for(let unit of playerArmy) {
+    unit.armyPosition = { // assign armyPosition
+      x,
+      y
+    };
+    if(i % 5 === 0) {
+      y += marginHeight;
+      x = defaultWidth;
+    } else {
+      x += marginWidth;
+    }
+    i++;
+  }
+  if(playerArmy.length !== 0) {
+    playerArmyPositionX = playerArmy[playerArmy.length - 1].armyPosition.x;
+    playerArmyPositionY = playerArmy[playerArmy.length - 1].armyPosition.y;
+  } else {
+    playerArmyPositionX = unitRosterWidth + 20;
+    playerArmyPositionY = titleHeight + 60;
+  }
+}
+
+export const rearrangeComputerArmyPosition = () => {
+  let marginWidth = armyImgWidth + 10;
+  let marginHeight = armyImgHeight + 15;
+  let defaultWidth = unitRosterWidth + 20;
+  let x:number= unitRosterWidth + 20;
+  let y:number= titleHeight + 60;
+  let i = 1;
+  for(let unit of computerArmy) {
+    unit.armyPosition = { // assign armyPosition
+      x,
+      y
+    };
+    if(i % 5 === 0) {
+      y += marginHeight;
+      x = defaultWidth;
+    } else {
+      x += marginWidth;
+    }
+    i++;
+  }
+  if(computerArmy.length !== 0) {
+    computerArmyPositionX = computerArmy[computerArmy.length - 1].armyPosition.x;
+    computerArmyPositionY = computerArmy[computerArmy.length - 1].armyPosition.y;
+  } else {
+    computerArmyPositionX = unitRosterWidth + 20;;
+    computerArmyPositionY = titleHeight + 60;
   }
 }
