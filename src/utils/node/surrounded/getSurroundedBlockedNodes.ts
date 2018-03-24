cecbe13488da0818742540f4ab5582d3af0de1ca
ualikhansars <valikhan754@gmail.com>
2018-03-24 07:46:05 -0700

    test getSurroundedBlockedNodes in surroundedNodesUtils

--- src/utils/node/surrounded/getSurroundedBlockedNodes.ts
+++ src/utils/node/surrounded/getSurroundedBlockedNodes.ts
@@ -7,20 +7,21 @@ import {
   addNodeIntoArray,
   deleteUnitFromArray
 } from '../../index';
+import Unit from '../../../unit/types/Unit';
 /*
  get closest to unit nodes where alliedUnits are located
  or moving to
 */
-export const getSurroundedBlockedNodes = (unit:any) => {
+export const getSurroundedBlockedNodes = (unit:Unit) => {
   let surroundedNodes = getSurroundedNodes(unit, 1);
-  let surroundedBlockedNodes:any = [];
-  let alliedUnits:any[];
+  let surroundedBlockedNodes:any[] = [];
+  let alliedUnits:Unit[];
   if(unit.controlBy === 'player') {
     alliedUnits = playerUnits;
   } else {
     alliedUnits = computerUnits;
   }
-  let updatedUnits = deleteUnitFromArray(unit, alliedUnits);
+  let updatedUnits:Unit[] = deleteUnitFromArray(unit, alliedUnits);
   for(let node of surroundedNodes) {
     for(let alliedUnit of updatedUnits) {
       if(alliedUnit.currentNode.x === node.x && alliedUnit.currentNode.y === node.y) {
---
+++ test/utils_test/node_test/surrounded_test/getSurroundedBlockedNodes_test.ts
@@ -0,0 +1,42 @@
+import {assert} from 'chai';
+import {getSurroundedBlockedNodes} from '../../../../src/utils/node/surrounded';
+import {createUnit} from '../../../../src/unit/create';
+import {removeUnit} from '../../../../src/store/unit/units';
+import Unit from '../../../../src/unit/types/Unit';
+
+describe('NodeUtils: surrounded test', () => {
+
+  describe('getSurroundedBlockedNodes test', () => {
+
+    // create units
+    const unit:Unit = createUnit('scouts', 320, 120, 5, 'computer');
+    const alliedUnit1:Unit = createUnit('scouts', 320, 80, 5, 'computer');
+    const alliedUnit2:Unit = createUnit('scouts', 280, 80, 5, 'computer');
+
+    alliedUnit2.setNextNode({x: 280, y: 120});
+
+    // remove units
+    after(() => {
+      removeUnit(unit);
+      removeUnit(alliedUnit1);
+      removeUnit(alliedUnit2);
+    });
+
+    it('Nodes {x: 320, y:80}, {x: 280, y: 80} and {x:280, y: 120} should be blocked', () => {
+      const blockedNodes:any[] = getSurroundedBlockedNodes(unit);
+      let pass:boolean = true;
+      for(let node of blockedNodes) {
+        if(!pass) break; // test not passed, abort loop
+        if(node.x === 320 && node.y === 80) { /* pass */ }
+        else if(node.x === 280 && node.y === 80) { /* pass */ }
+        else if(node.x === 280 && node.y === 120) { /* pass */ }
+        else {
+          /* not passed */
+          pass = false;
+        }
+      }
+
+      assert.equal(pass, true);
+    });
+  });
+});
