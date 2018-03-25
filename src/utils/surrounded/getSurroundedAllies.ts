71fb98cf71491ce2a956ac45ae5f2c3c6b6b49f0
ualikhansars <valikhan754@gmail.com>
2018-03-25 00:38:32 -0700

    test getSurroundedAllies in surroundedUtils

--- src/utils/surrounded/getSurroundedAllies.ts
+++ src/utils/surrounded/getSurroundedAllies.ts
@@ -1,15 +1,30 @@
-import {computerUnits} from '../../store/unit/units';
+import {
+  computerUnits,
+  playerUnits
+} from '../../store/unit/units';
 import {getSurroundedNodes} from './getSurroundedNodes';
 import {getNodeFromMap} from '../node/get/fromMap';
+import {deleteUnitFromArray} from '../unit/utils';
 import Unit from '../../unit/types/Unit';
 
 export const getSurroundedAllies = (unit:Unit):Unit[] => {
   let surroundedAllies:Unit[] = [];
+  let allies:Unit[] = [];
+  let updatedAllies:Unit[] = [];
+  if(unit.controlBy === 'computer') {
+    allies = Object.assign([], computerUnits);
+  } else {
+    allies = Object.assign([], playerUnits);
+  }
+
+  updatedAllies = deleteUnitFromArray(unit, allies);
+
   let nodes = getSurroundedNodes(unit, 3);
   for(let node of nodes) {
-    for(let enemy of computerUnits) {
-      if(node.x === enemy.x && node.y === enemy.y) {
-        surroundedAllies.push(enemy);
+    for(let ally of updatedAllies) {
+      let allyNode = getNodeFromMap(ally.x, ally.y);
+      if(node.x === allyNode.x && node.y === allyNode.y) {
+        surroundedAllies.push(ally);
       }
     }
   }
--- test/utils_test/surrounded_test/getSurroundedAllies_test.ts
+++ test/utils_test/surrounded_test/getSurroundedAllies_test.ts
@@ -1,9 +1,41 @@
 import {assert} from 'chai';
 import {getSurroundedAllies} from '../../../src/utils/surrounded';
+import {createUnit} from '../../../src/unit/create';
+import {removeUnit} from '../../../src/store/unit/units';
+import Unit from '../../../src/unit/types/Unit';
 
 describe('surroundedUtils test', () => {
 
   describe('getSurroundedAllies test', () => {
-    
+    let unit:Unit = createUnit('lightInfantry', 280, 520, 5, 'player');
+    let ally1:Unit = createUnit('lightInfantry', 280, 440, 5, 'player');
+    let ally2:Unit = createUnit('lightInfantry', 760, 320, 5, 'player');
+    let ally3:Unit = createUnit('lightInfantry', 320, 520, 5, 'player');
+
+    let ally1ID:number = ally1.id;
+    let ally2ID:number = ally2.id;
+    let ally3ID:number = ally3.id;
+
+    // remove units
+    after(() => {
+      removeUnit(unit);
+      removeUnit(ally1);
+      removeUnit(ally2);
+      removeUnit(ally3);
+    });
+
+    it('ally1 and ally3 should be in the surroundedAllies', () => {
+      const surroundedAllies:Unit[] = getSurroundedAllies(unit);
+      let pass:boolean = true;
+      for(let ally of surroundedAllies) {
+        if(ally.id === ally1ID || ally.id === ally3ID) {
+           // pass
+        } else {
+          pass = false;
+        }
+      }
+
+      assert.equal(pass, true);
+    });
   });
 })
