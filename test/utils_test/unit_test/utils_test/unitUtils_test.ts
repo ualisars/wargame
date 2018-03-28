2d3b9897edcc8bf34c8c6a2aa308578aaf83b85f
ualikhansars <valikhan754@gmail.com>
2018-03-28 10:09:23 -0700

    test isUnitInArray in unitUtils

--- test/utils_test/unit_test/utils_test/unitUtils_test.ts
+++ test/utils_test/unit_test/utils_test/unitUtils_test.ts
@@ -2,7 +2,8 @@ import {assert} from 'chai';
 import {
   addUnitIntoArray,
   deleteUnitFromArray,
-  getDistanceBetweenTwoUnitsInGrids
+  getDistanceBetweenTwoUnitsInGrids,
+  isUnitInArray
 } from '../../../../src/utils/unit/utils';
 import Unit from '../../../../src/unit/types/Unit';
 import {createUnit} from '../../../../src/unit/create';
@@ -55,9 +56,41 @@ describe('UnitUtils utils test', () => {
     let testUnit1:Unit = createUnit('scouts', 240, 360, 5, 'computer');
     let testUnit2:Unit = createUnit('scouts', 440, 320, 5, 'computer');
 
+    after(() => {
+      removeUnit(testUnit1);
+      removeUnit(testUnit2);
+    });
+
     it('distance between units should be 6 grids', () => {
       let distance:number = getDistanceBetweenTwoUnitsInGrids(testUnit1, testUnit2);
       assert.equal(distance, 6);
     });
+  });
+
+  describe('isUnitInArray', () => {
+    let testUnits:Unit[] = [];
+    let u1:Unit = createUnit('scouts', 240, 360, 5, 'computer');
+    let u2:Unit = createUnit('scouts', 240, 360, 5, 'computer');
+    let u3:Unit = createUnit('scouts', 240, 360, 5, 'computer');
+
+    testUnits.push(u1);
+    testUnits.push(u3);
+    it('u1 and u3 should be the testUnits array', (done) => {
+      let u1Found:boolean = false;
+      let u2Found:boolean = false;
+      let u3Found:boolean = false;
+
+      for(let unit of testUnits) {
+        if(unit.id === u1.id) u1Found = true;
+        else if(unit.id === u2.id) u2Found = true;
+        else if(unit.id === u3.id) u3Found = true;
+      }
+
+      assert.equal(u1Found, true);
+      assert.equal(u2Found, false);
+      assert.equal(u3Found, true);
+
+      done();
+    })
   });
 });
