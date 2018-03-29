6f4e8e78a2f63c1ba47f804f3d88b74632c9b68b
ualikhansars <valikhan754@gmail.com>
2018-03-29 09:04:25 -0700

    test getClosestUnitToNode in actionUnitUtils

---
+++ test/utils_test/unit_test/actions_test/unitActions_test.ts
@@ -0,0 +1,33 @@
+import {assert} from 'chai';
+
+import {createUnit} from '../../../../src/unit/create';
+import {
+  getClosestUnitToNode
+} from '../../../../src/utils/unit/actions';
+import {removeUnit} from '../../../../src/store/unit/units';
+import Unit from '../../../../src/unit/types/Unit';
+
+
+describe('unitUtils: actions test', () => {
+
+  describe('getClosestUnitToNode test', () => {
+    let node:any = {x: 400, y: 160};
+    let unit1:Unit = createUnit('scouts', 280, 200, 5, 'computer');
+    let unit2:Unit = createUnit('scouts', 480, 80, 5, 'computer');
+    let unit3:Unit = createUnit('scouts', 480, 160, 5, 'computer');
+
+    // remove units after test completed
+    after(() => {
+      removeUnit(unit1);
+      removeUnit(unit2);
+      removeUnit(unit3);
+    });
+
+    it('unit3 with current node {x: 480, y: 160} should be closest to node', () => {
+      let closestUnit:Unit = getClosestUnitToNode(node);
+      assert.notEqual(closestUnit.id, unit1.id);
+      assert.notEqual(closestUnit.id, unit2.id);
+      assert.equal(closestUnit.id, unit3.id);
+    });
+  });
+});
