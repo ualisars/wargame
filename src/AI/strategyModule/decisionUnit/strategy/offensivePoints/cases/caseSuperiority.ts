2dad3582b2a992be6601998175a3eb838d32a330
ualikhansars <valikhan754@gmail.com>
2018-08-02 11:37:10 -0400

    add lotOfCavalry in caseSuperiority

--- src/AI/strategyModule/decisionUnit/strategy/offensivePoints/cases/caseSuperiority.ts
+++ src/AI/strategyModule/decisionUnit/strategy/offensivePoints/cases/caseSuperiority.ts
@@ -3,9 +3,9 @@ import {
     getRandomValueInRangeWithProbability
 } from '../../../../../../utils/random';
 import '../../../../../../store/AI/translators/totalAdvantage/totalAdvantageTranslator';
-import { playerHasTooManySkirmishers, playerHasManySkirmishers, playerHasFewSkirmishers, computerHasTooManySkirmishers, computerHasManySkirmishers } from '../../../../../../store/AI/translators/typesTranslators/skirmisherTranslatorStore/skirmisherTranslatorStore';
-import { computerHasTooManyCavalry, computerHasManyCavalry } from '../../../../../../store/AI/translators/typesTranslators/cavalryTranslatorStore/cavalryTranslator';
-import { playerHasTooManySpearmen, playerHasManySpearmen } from '../../../../../../store/AI/translators/typesTranslators/spearmenTranslatorStore/spearmenTranslator';
+import { playerHasTooManySkirmishers, playerHasManySkirmishers, playerHasFewSkirmishers, computerHasTooManySkirmishers, computerHasManySkirmishers, playerHasLotsOfSkirmishers, computerHasLotsOfSkirmishers } from '../../../../../../store/AI/translators/typesTranslators/skirmisherTranslatorStore/skirmisherTranslatorStore';
+import { computerHasTooManyCavalry, computerHasManyCavalry, computerHasLotsOfCavalry } from '../../../../../../store/AI/translators/typesTranslators/cavalryTranslatorStore/cavalryTranslator';
+import { playerHasTooManySpearmen, playerHasManySpearmen, playerHasLotsOfSpearmen } from '../../../../../../store/AI/translators/typesTranslators/spearmenTranslatorStore/spearmenTranslator';
 
 /*
     this function return ratio in range from 90 to 100
@@ -15,17 +15,11 @@ export const caseSuperiority = (): number => {
 
     let offensivePoints;
 
-    if( 
-        (playerHasTooManySkirmishers || playerHasManySkirmishers) &&
-        (computerHasTooManyCavalry || computerHasManyCavalry) 
-    ) {
+    if(playerHasLotsOfSkirmishers && computerHasLotsOfCavalry) {
         offensivePoints = 100;
     }
 
-    else if(
-        (computerHasTooManySkirmishers || computerHasManySkirmishers) &&
-        (playerHasTooManySpearmen || playerHasManySpearmen) 
-    ) {
+    else if(computerHasLotsOfSkirmishers && playerHasLotsOfSpearmen) {
         offensivePoints = 100;
     }
 
