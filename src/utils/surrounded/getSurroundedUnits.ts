2d4d38fc2d6c942651226608855f8c9b60497059
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-11-03 08:43:27 -0700

    implement assessVulnerability method

---
+++ src/utils/surrounded/getSurroundedUnits.ts
@@ -0,0 +1,33 @@
+import {
+    computerUnits,
+    playerUnits
+  } from '../../store/unit/units';
+  import {getSurroundedNodes} from './getSurroundedNodes';
+  import {getNodeFromMap} from '../node/get/fromMap';
+  import {deleteUnitFromArray, isUnitInArray} from '../unit/general';
+  import Unit from '../../unit/types/Unit';
+  
+  export const getSurroundedUnits = (
+      unit: Unit, 
+      units: Unit[], 
+      range: number = 1, 
+      countOneself: false
+    ): Unit[] => {
+    let surroundedUnits:Unit[] = [];
+    let updatedUnits:Unit[] = Object.assign([], units);
+    
+    if(!countOneself && isUnitInArray(unit, updatedUnits)) {
+        updatedUnits = deleteUnitFromArray(unit, updatedUnits);
+    }
+    
+    let nodes = getSurroundedNodes(unit, range);
+    for(let node of nodes) {
+      for(let currentUnit of units) {
+        let currentUnitNode = getNodeFromMap(currentUnit.x, currentUnit.y);
+        if(node.x === currentUnitNode.x && node.y === currentUnitNode.y) {
+          surroundedUnits.push(currentUnit);
+        }
+      }
+    }
+    return surroundedUnits;
+  }
