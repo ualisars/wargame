bf5fb6688bb7cdad2bb55fd5bc3ca599d24f178c
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-11-10 08:47:23 -0800

    implement calcalateTotalVulnerability

--- src/AI/strategyModule/decisionUnit/bestPositionAlgorithm/bestPosition.ts
+++ src/AI/strategyModule/decisionUnit/bestPositionAlgorithm/bestPosition.ts
@@ -1,6 +1,18 @@
 import { computerUnits } from "../../../../store/unit/units";
+import { initialMap } from "../../../../map/createMap/initialMap";
+import PriorityQueue from "../../../../store/queues/PriorityQueue";
 
 
 export const bestPosition = () => {
-    
+    let positionsQueue = new PriorityQueue();
+    for(let unit of computerUnits) {
+        for(let node of initialMap) {
+            
+            positionsQueue.insert
+        }
+    }
+}
+
+const addAllUnitPositions = () => {
+
 }
