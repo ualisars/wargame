1c89a007c7089e6950b1131d85cf6098c7aad61a
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-12-08 00:45:14 -0800

    map unit icons to units

--- src/store/unit/currentlyChosenUnit.ts
+++ src/store/unit/currentlyChosenUnit.ts
@@ -3,7 +3,6 @@ import Unit from '../../unit/types/Unit';
 export let currentlyChosenUnit:Unit = null;
 
 export const assignCurrentlyChosenUnit = (unit:Unit) => {
-  // check unit
   if(unit) {
       currentlyChosenUnit = unit;
   } else {
--- src/unit/choose/onChooseUnit.ts
+++ src/unit/choose/onChooseUnit.ts
@@ -1,6 +1,8 @@
-import {gridSize} from '../../config';
+import {gridSize, CANVAS_HEIGHT} from '../../config';
 import {assignCurrentlyChosenUnit} from '../../store/unit/currentlyChosenUnit';
 import Unit from '../types/Unit';
+import { playerUnits } from '../../store/unit/units';
+import { UNIT_LEFT_PANEL_WIDTH, UNIT_ICON_WIDTH, UNIT_PANEL_HEIGHT, UNIT_ICON_HEIGHT } from '../../UI/battleUI/unitPanel/settings';
 
 
 export const onChooseUnit = (units:Unit[], mouseX:number, mouseY:number) => {
@@ -12,6 +14,21 @@ export const onChooseUnit = (units:Unit[], mouseX:number, mouseY:number) => {
       console.log('unit', unit.name, ' was chosen');
       unit.isCurrentlyChosen = true;
       foundUnit = unit;
+      break;
+    }
+  }
+
+  let startX = UNIT_LEFT_PANEL_WIDTH;
+  let y0 = CANVAS_HEIGHT - UNIT_PANEL_HEIGHT;
+  let y1 = y0 + UNIT_ICON_HEIGHT;
+  for(let i = 0; i < playerUnits.length; ++i) {
+    let x0 = startX + (UNIT_ICON_WIDTH * (i + 1));
+    let x1 = x0 + UNIT_ICON_WIDTH;
+    if(mouseX >= x0 && mouseX < x1 && mouseY >= y0 && mouseY < y1) {
+      let unit = playerUnits[i];
+      unit.isCurrentlyChosen = true;
+      foundUnit = unit;
+      break;
     }
   }
   assignCurrentlyChosenUnit(foundUnit);
