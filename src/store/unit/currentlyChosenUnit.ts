ecade585bdb35e6123ab4b24a1a9867038f224b1
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2019-01-06 09:11:55 -0800

    prevent selecting computer units

--- src/store/unit/currentlyChosenUnit.ts
+++ src/store/unit/currentlyChosenUnit.ts
@@ -3,7 +3,7 @@ import Unit from '../../unit/types/Unit';
 export let currentlyChosenUnit:Unit = null;
 
 export const assignCurrentlyChosenUnit = (unit:Unit) => {
-  if(unit) {
+  if(unit && unit.controlBy === "player") {
       currentlyChosenUnit = unit;
   } else {
     currentlyChosenUnit = null;
